74f827e8393cb4b483e2cbe7a6b4b441903b9538
gmorde <giladmorde@gmail.com>
2016-05-15 15:41:34 +0300

    postServiceChanges

--- public/javascripts/services/posts.js
+++ public/javascripts/services/posts.js
@@ -1,13 +1,11 @@
-app.factory('posts', [function(){
-  var posts = [{
-      title: 'this is hardcoded', 
-      upvotes: 5,
-      comments: [
-        { author: 'Joe', body: 'Cool post!', upvotes: 0 },
-        { author: 'Bob', body: 'Bad post!', upvotes: 3 },
-        { author: 'Jim', body: 'Ok post!', upvotes: 1 }
-      ]
-    }]
+app.factory('posts', ['$http', function($http) {
+  var postService = {
+    posts: [],
 
-  return posts;
+    getAll: function() {
+      // blank fo rnow
+    }
+  };
+  
+  return postService;
 }]);
